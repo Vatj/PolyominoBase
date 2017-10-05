08d005a30137e8673f36e93111ddfe4c36461ebc
Alex Leonard <tkom343@gmail.com>
2017-10-05 16:19:47 +0100

    Fitness updates

--- scripts/fitness_plotting.py
+++ scripts/fitness_plotting.py
@@ -8,8 +8,8 @@ icy.Use_Seaborn()
 
 from matplotlib.animation import FuncAnimation,ImageMagickWriter
 
-RUNS=1000
-RUN_TYPE=4
+RUNS=250
+RUN_TYPE=3
 
 def SetRuns(sr):
     global RUNS
@@ -29,66 +29,57 @@ def SetPar(st,sr):
 
 
 
-def AddNewDataToFile(classifier,data_in,needle):
-    with open('/rscratch/asl47/Bulk_Run/Modular/Evolution_Solution_Times_Needle{}.txt'.format(needle), "a") as data_file:
+def AddNewDataToFile(classifier,data_in,needle,mu):
+    with open('/rscratch/asl47/Processed/Dynamic/Evolution_Solution_Times_Mu{}_Needle{}.txt'.format(mu,needle), "a") as data_file:
         data_file.write(classifier+': ')
         for value in data_in:
             data_file.write('{} '.format(value))
         data_file.write('\n')
 
-def AddBulkDataToFile(Ts,Os,needle):
+def AddBulkDataToFile(Ts,Os,needle,mu):
     keys=['T{}{}'.format(t,'' if t==3 else 'O{}'.format(o)) for (t,o) in zip(Ts,Os)]
-    SetRuns(1000)
+    SetRuns(250)
     
     for key in keys:
         SetType(int(key[1]))
-        data=MaxFraction(needle, 1 if key[1]=='3' else 3,'' if key[1]=='3' else int(key[3:]))
-        AddNewDataToFile(key,data,needle)
+        data=MaxFraction(needle,mu,'' if key[1]=='3' else int(key[3:]))
+        AddNewDataToFile(key,data,needle,mu)
         
-def LoadExistingData(needle):
-    data_dict={key:[int(v) for v in values.split()] for (key,values) in [line.rstrip('\n').split(':') for line in open('/rscratch/asl47/Bulk_Run/Modular/Evolution_Solution_Times_Needle{}.txt'.format(needle))]}
+def LoadExistingData(needle,mu):
+    data_dict={key:[int(v) for v in values.split()] for (key,values) in [line.rstrip('\n').split(':') for line in open('/rscratch/asl47/Processed/Dynamic/Evolution_Solution_Times_Mu{}_Needle{}.txt'.format(mu,needle))]}
     return data_dict
 
     
-def MaxFraction(needle_length=30,slicer=1,O=3,mu=4):
+def MaxFraction(needle_length=30,mu=4,O=3,slicer=0):
     occ=0
     bounce=0
     firsts=[]
-    maxes=[]
     additional=''
-    if RUN_TYPE==4:
+    if RUN_TYPE==4 or RUN_TYPE==5:
         additional='_O{}'.format(O)
 
-    Mu_Sets={4:'0.003125',2:'0.006250',1:'0.012500'}
-    Mu_Files={4:'',2:'Mu2',1:'Mu1'}
+    Mu_Sets={32:'0.001563',16:'0.003125',8:'0.006250',4:'0.012500',1:'0.05000'}
+    #Mu_Files={4:'',2:'Mu2',1:'Mu1'}
     
     
     needle=np.array([1]*needle_length,dtype=np.float64)
     t=0
     for r in xrange(RUNS):
-        #subfile_name='Modular{}_T20_C200_N1000_Mu{}{}_B5000_Run{}'.format(RUN_TYPE,Mu_Sets[mu],additional,r)
-        #fitness_import=np.genfromtxt('/scratch/asl47/Data_Runs/Dynamic/T{}{}{}/{}_Fitness.txt'.format(RUN_TYPE,additional[1:],Mu_Files[mu],subfile_name),dtype=np.float64)
-        fitness_import=np.genfromtxt('/rscratch/asl47/Bulk_Run/Modular/Modular3_T20_C200_N500_Mu{}_B10000_Run{}_Fitness.txt'.format(Mu_Sets[mu],r),dtype=np.float64)
-        maxes.append(max(fitness_import[:,1]))
+        subfile_name='Modular{}_T20_C200_N500_Mu{}{}_K15000_Run{}'.format(RUN_TYPE,Mu_Sets[mu],additional,r)
+        fitness_import=np.genfromtxt('/scratch/asl47/Data_Runs/Dynamic_2/T{}Mu{}{}/{}_Fitness.txt'.format(RUN_TYPE,mu,additional[1:],subfile_name),dtype=np.float64)
+        #maxes.append(max(fitness_import[:,1]))
         if max(fitness_import[:,slicer])>=1:
             haystack=search_sequence_numpy(fitness_import[:,slicer],needle)
             if haystack>0:
                 occ+=1
                 firsts.append(haystack)
-                #firsts.append(np.argmin(fitness_import[:,1]<1))
 
-            #if np.argmin(fitness_import[:,1]<1) < (fitness_import.shape[0]-1-np.argmax(fitness_import[::-1,1]<1)):
-            #    bounce+=1
 
     print "counted runs: ",t
     print "seen {} times for a fraction of {}".format(occ,occ*1./RUNS)
-    #print "ended fit {} times".format(ends)
-    print "bounced {} times".format(bounce)
+    #print "bounced {} times".format(bounce)
 
-
-    #plt.hist(firsts,range=(0,5000),bins=20)
-    #plt.show(block=False)
-    return firsts#,maxes
+    return firsts
 
 import scipy as sp
 import scipy.optimize
@@ -121,40 +112,84 @@ def PlotHistogram(data,bins='empty',c_in='hotpink',mark_in='o',label_in='Default
     #return (histC,xs)
 
 
-def PlotPartialHistogram(data,bins='empty',c_in='hotpink',mark_in='o',label_in='Default'):
+def PlotPartialHistogram(data,bins='empty',c_in='hotpink',mark_in='o',label_in='Default',label_on=True):
     if bins=='empty':
         bins=int(np.sqrt(len(data)+0.5))
-    hist,bins=np.histogram(data,bins=np.logspace(2,np.log10(5000),bins))#np.log10(min(data)*0.9)
-    return plt.scatter(np.mean(zip(bins,bins[1:]),axis=1),np.cumsum(hist)/(1.*RUNS),c=c_in,marker=mark_in,s=50,label=label_in)
+    hist,bins=np.histogram(data,bins=np.logspace(2,np.log10(15000),bins))#np.log10(min(data)*0.9)
+    if label_on:
+        return plt.scatter(np.mean(zip(bins,bins[1:]),axis=1),np.cumsum(hist)/(1.*RUNS),c=c_in,marker=mark_in,s=50,label=label_in)
+    else:
+        return plt.scatter(np.mean(zip(bins,bins[1:]),axis=1),np.cumsum(hist)/(1.*RUNS),c=c_in,marker=mark_in,s=50)
 
 def GetHistCoords(data,bins):
-    hist,bin_edges=np.histogram(data,bins=np.logspace(2,np.log10(5000),bins))
+    hist,bin_edges=np.histogram(data,bins=np.logspace(2,np.log10(15000),bins))
     return zip(np.mean(zip(bin_edges,bin_edges[1:]),axis=1),np.cumsum(hist)/(1.*RUNS))
 
 
-def PlotManyHistograms(data_dict,bins=35):
-    needle=''
+plot_params={
+    'T3':{'mark_in':'x','c_in':'darkgreen','label_in':'Static'},
+    'T4O5':{'mark_in':'o','c_in':'darkred','label_in':r'Dynamic ($\Omega^2$ = 5)'},
+    'T4O25':{'mark_in':'s','c_in':'tomato','label_in':r'Dynamic ($\Omega^2$ = 25)'},
+    'T4O125':{'mark_in':'h','c_in':'gold','label_in':r'Dynamic ($\Omega^2$ = 125)'},
+    'T5O5':{'mark_in':'<','c_in':'skyblue','label_in':r'Dynamic ($\Omega^1$ = 5)'},
+    'T5O25':{'mark_in':'>','c_in':'slateblue','label_in':r'Dynamic ($\Omega^1$ = 25)'},
+    'T5O125':{'mark_in':'^','c_in':'navy','label_in':r'Dynamic ($\Omega^1$ = 125)'},
+    'T4O250':{'mark_in':'+','c_in':'olive','label_in':r'Dynamic ($\Omega$ = 250)'},
+}
+
+def PlotFilledFractions(needles,mu):
+    bins=30
+    plt.figure()
+    data_frame_low={}
+    data_frame_high={}
+    data_frame_locs=0
+    for needle in needles:
+        data_dict=LoadExistingData(needle,mu)
+        sorted_keys=sorted(data_dict.keys())
+        sorted_keys[1:4]=sorted(sorted_keys[1:4],key=lambda x: int(x[3:]))
+        sorted_keys[4:]=sorted(sorted_keys[4:],key=lambda x: int(x[3:]))
+        for run_t in sorted_keys:
+            PlotPartialHistogram(data_dict[run_t],bins,label_on=False,**plot_params[run_t])
+            if needle==needles[0]:
+                data_frame_locs,data_frame_low[run_t]=zip(*GetHistCoords(data_dict[run_t],bins))
+
+            elif needle==needles[-1]:
+                data_frame_locs,data_frame_high[run_t]=zip(*GetHistCoords(data_dict[run_t],bins))
+                plt.fill_between(data_frame_locs,data_frame_low[run_t],data_frame_high[run_t],alpha=0.5,label=plot_params[run_t]['label_in'],color=plot_params[run_t]['c_in'])
+
+
+        
+
+        
+
+    plt.plot([15000,15000],[-1,2],'k-',lw=3)
+    plt.ylim([-0.05,1.05])
+    plt.xlabel(r'Generation')
+    plt.ylabel('Solution CDF')
+    plt.xscale('log')
+    plt.title(r'$\langle \mu L \rangle$ = {}, Sustained $g$ = [{}]'.format(4./mu,', '.join(map(str,needles))))
+    plt.legend(ncol=2)
+    plt.tight_layout()
+    plt.show(block=False)
+    
+
+def PlotManyHistograms(needle,mu,bins=35):
+    data_dict=LoadExistingData(needle,mu)
     if type(data_dict)==int:
         needle=data_dict
-        data_dict=LoadExistingData(needle)
+        data_dict=LoadExistingData(needle,mu)
         
-    SetRuns(1000)
-    needle_list=[50]#1,10,25,50,75,100,300]
+    SetRuns(250)
+    needle_list=[5,25,50,100,500]#1,10,25,50,75,100,300]
 
-    plot_params={'T3':{'mark_in':'o','c_in':'firebrick','label_in':'Static'},
-                 'T4O1':{'mark_in':'s','c_in':'darkgreen','label_in':r'Dynamic ($\Omega$ 1)'},
-                 'T4O3':{'mark_in':'v','c_in':'royalblue','label_in':r'Dynamic ($\Omega$ 3)'},
-                 'T4O10':{'mark_in':'^','c_in':'darkgoldenrod','label_in':r'Dynamic ($\Omega$ 10)'},
-                 'T4O25':{'mark_in':'<','c_in':'darkslategray','label_in':r'Dynamic ($\Omega$ 25)'},
-                 'T4O75':{'mark_in':'>','c_in':'indigo','label_in':r'Dynamic ($\Omega$ 75)'},
-                 'T4O150':{'mark_in':'h','c_in':'navy','label_in':r'Dynamic ($\Omega$ 150)'},
-                 'T4O250':{'mark_in':'+','c_in':'olive','label_in':r'Dynamic ($\Omega$ 250)'},
-                 }
+    
     sorted_keys=sorted(data_dict.keys())
-    sorted_keys[1:]=sorted(sorted_keys[1:],key=lambda x: int(x[3:]))
+    sorted_keys[1:4]=sorted(sorted_keys[1:4],key=lambda x: int(x[3:]))
+    sorted_keys[4:]=sorted(sorted_keys[4:],key=lambda x: int(x[3:]))
     fig=plt.figure(figsize=(10,10))
     scatters=[]
     for run_t in sorted_keys:
+        print run_t
         scatters.append(PlotPartialHistogram(data_dict[run_t],bins,**plot_params[run_t]))
         
     plt.plot([5000,5000],[-1,2],'k-',lw=3)
@@ -162,13 +197,15 @@ def PlotManyHistograms(data_dict,bins=35):
     plt.xlabel(r'Generation')
     plt.ylabel('Solution CDF')
     plt.xscale('log')
-    plt.title('Needles: [{}]'.format(', '.join(map(str,needle_list))))
+    plt.title('Mu: {}'.format(mu))
     time_text = plt.text(70, 0.25, 'needle: 1',horizontalalignment='left', verticalalignment='center',)
     plt.legend(ncol=2)
     plt.tight_layout()
+    #plt.show(block=False)
+    #return
 
     def update_plot(i,needles,scatter_list):
-        data_dict=LoadExistingData(needles[i])
+        data_dict=LoadExistingData(needles[i],mu)
         time_text.set_text('needle: {}'.format(needles[i]))
         for scatter,key in zip(scatter_list,sorted_keys):
             scatter.set_offsets(GetHistCoords(data_dict[key],bins))
@@ -176,9 +213,9 @@ def PlotManyHistograms(data_dict,bins=35):
     def init():
         pass
 
-    anim = FuncAnimation(fig, update_plot,init_func=init,frames=len(needle_list), interval=2500, blit=False,fargs=(needle_list,scatters),repeat=True)
-    writer = ImageMagickWriter(fps=0.75)
-    anim.save('Solutions_Slow.gif', writer=writer)
+    anim = FuncAnimation(fig, update_plot,init_func=init,frames=len(needle_list), interval=1500, blit=False,fargs=(needle_list,scatters),repeat=True)
+    #writer = ImageMagickWriter(fps=0.75)
+    #anim.save('Solutions_Slow.gif', writer=writer)
     
     plt.show(block=False)
 
