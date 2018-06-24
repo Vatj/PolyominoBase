6f9af44d0c7beb9bb08f430e4dde5c865c315b7a
Victor Jouffrey <vatj2@pc15.tcm.phy.cam.ac.uk>
2018-06-24 13:44:03 +0100

    Duplication is now less painful to run

--- src/genotype_mains.cpp
+++ src/genotype_mains.cpp
@@ -4,7 +4,7 @@ namespace simulation_params
 {
   uint8_t n_genes=3, colours=7, metric_colours=9;
   uint8_t phenotype_builds=40;
-  uint32_t n_samples = 10, n_jiggle = 200;
+  uint32_t n_samples = 10, n_jiggle = 1000;
   std::mt19937 RNG_Engine(std::random_device{}());
   double UND_threshold=0.25;
   bool allow_duplicates = true, STERIC_FORBIDDEN = false;
@@ -27,9 +27,9 @@ namespace io_params
   std::string set_metric_file = file_path + "SetMetrics" + file_details + extra + ending;
   std::string genome_metric_file = file_path + "GenomeMetrics" + file_details + extra + ending;
 
-  std::string file_path2 = "/rscratch/vatj2/public_html/Polyominoes/data/gpmap/V6/duplication/";
-  std::string file_details2 = "_N" + std::to_string(simulation_params::n_genes) + "_C" + std::to_string(simulation_params::colours) + threshold + builds;
-  // std::string duplicate_file = file_path2 + "DuplicateGenotypes" + file_details2 + iso_ending;
+  // std::string file_path2 = "/rscratch/vatj2/public_html/Polyominoes/data/gpmap/V6/duplication/";
+  std::string file_details2 = "_N" + std::to_string(simulation_params::n_genes + 1) + "_C" + std::to_string(simulation_params::colours) + threshold + builds;
+  // std::string duplicate_file = file_path + "DuplicateGenotypes" + file_details2 + ending;
   std::string duplicate_file = file_path + "JiggleDuplicateGenotypes" + file_details2 + extra + ending;
   std::string dup_set_metric_file = file_path + "JiggleDuplicateSetMetrics" + file_details2 + extra + ending;
   std::string dup_genome_metric_file = file_path + "JiggleDuplicateGenomeMetrics" + file_details2 + extra + ending;
@@ -42,16 +42,12 @@ namespace io_params
 int main()
 {
   std::cout << "Global path : " << io_params::file_path  << "\n";
-  std::cout << "n_genes : " <<+ simulation_params::n_genes;
-  std::cout << " colours : " <<+ simulation_params::colours;
-  std::cout << " metric_colours : " <<+ simulation_params::metric_colours << "\n";
 
   // JustExhaustive();
   // ExhaustiveMetricsPrintAll();
-  QuickFromFile();
+  // QuickFromFile();
   // QuickRandom();
-  // Duplicate();
-  // DuplicateJiggle();
+  DuplicateJiggle();
   // AnalysisJiggleFromFile();
 
   std::cout << "Back to sleep!" << std::endl;
@@ -141,37 +137,27 @@ void QuickFromFile()
   std::cout << "The phenotype table has " <<+ nphen << " entries \n";
 }
 
-void Duplicate()
-{
-  std::vector<Genotype> genomes, duplicates;
-
-  LoadGenomeFile(io_params::genome_file, genomes);
-  duplicates = GenomesDuplication(genomes);
-  PrintGenomeFile(io_params::duplicate_file, duplicates);
-}
-
 void DuplicateJiggle()
 {
+  PhenotypeTable pt, pt2;
   std::vector<Genotype> genomes, genomes_jiggle, duplicates;
   Set_to_Genome set_to_genome;
   std::vector<Set_Metrics> metrics;
 
   LoadGenomeFile(io_params::genome_file, genomes);
   GenomesJiggleDuplication(genomes, genomes_jiggle, duplicates);
-  PrintGenomeFile(io_params::duplicate_file, duplicates);
-  PrintGenomeFile(io_params::jiggle_file, genomes_jiggle);
-}
-
-void AnalysisJiggleFromFile()
-{
-  PhenotypeTable pt;
-  std::vector<Genotype> genomes;
-  Set_to_Genome set_to_genome;
-  std::vector<Set_Metrics> metrics;
+  genomes.clear();
 
   LoadPhenotypeTable(io_params::phenotype_file, &pt);
-  LoadGenomeFile(io_params::duplicate_file, genomes);
-  PreProcessSampled(genomes, set_to_genome, &pt);
+  PreProcessSampled(genomes_jiggle, set_to_genome, &pt);
   GP_MapSimple(metrics, set_to_genome, &pt);
+  PrintMetrics(io_params::jiggle_set_metric_file, io_params::jiggle_genome_metric_file, metrics);
+
+  simulation_params::n_genes++;
+  genomes_jiggle.clear(), set_to_genome.clear(), metrics.clear();
+
+  LoadPhenotypeTable(io_params::phenotype_file, &pt2);
+  PreProcessSampled(duplicates, set_to_genome, &pt2);
+  GP_MapSimple(metrics, set_to_genome, &pt2);
   PrintMetrics(io_params::dup_set_metric_file, io_params::dup_genome_metric_file, metrics);
 }
